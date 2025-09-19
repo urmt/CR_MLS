2039db9b75269a59d4f58f9fb4263e0305888ab2
I Am <30534610+urmt@users.noreply.github.com>
2025-09-18 19:11:23 -0600

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:500
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+})
