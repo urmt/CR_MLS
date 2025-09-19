20ae4770e9d0a8e3a0404ba90fc0e3b2053f712d
I Am <30534610+urmt@users.noreply.github.com>
2025-09-18 20:12:06 -0600

    Update propertyRoutes.ts

--- server/src/routes/propertyRoutes.ts
+++ server/src/routes/propertyRoutes.ts
@@ -3,3 +3,7 @@ import { checkPropertyAccess } from '../controllers/propertyController'
 
 // Add to existing routes
 router.get('/:id/access', protect, checkPropertyAccess)
+import { verifyProperty } from '../controllers/propertyController'
+
+// Add to routes
+router.get('/:id/verify', verifyProperty)
